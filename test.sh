eba365ba9209eea943aa912d693cf9ac9fc683fa
gaidardzhiev <1v4n@protonmail.com>
2025-08-08 11:51:18 +0000

    Fri Aug  8 11:51:18 AM UTC 2025

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+Z=625
+
+X() {
+	od -An -tx1 | tr -d ' \n'
+}
+
+Y=0
+while [ "$Y" -lt "$Z" ]
+do
+	./getprand | tr -d '\n\r' | X || exit 1
+	Y=$((Y + 1))
+done | xxd -r -p | rngtest -c $(( (Z * 32) / 2500 ))
